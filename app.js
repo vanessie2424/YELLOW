7030631eae6d0d6bf19dd1c0966c7a8c4ee49132
Anazodo Vanessa <anazodovanessa19@gmail.com>
2023-07-29 17:01:52 +0100

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+
+    burger.addEventListener('click', ()=>{
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+         //Burger animation
+        burger.classList.toggle('toggle');
+    })
+
+   
+}
+
+navSlide();
