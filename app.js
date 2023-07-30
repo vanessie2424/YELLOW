a9e6a8c51b1fc4b7cc4ca05fd9d973912912b765
Anazodo Vanessa <anazodovanessa19@gmail.com>
2023-07-30 18:35:12 +0100

    init

--- app.js
+++ app.js
@@ -15,3 +15,22 @@ const navSlide = ()=>{
 }
 
 navSlide();
+
+// toggle menu for login and register
+const LoginForm = document.getElementById('LoginForm');
+const RegForm = document.getElementById('RegForm');
+const Indicator = document.getElementById('indicator');
+
+function register() {
+    RegForm.style.transform = 'translateX(0px)';
+    LoginForm.style.transform = 'translateX(0px)';
+    Indicator.style.transform = 'translateX(100px)';
+}
+
+function login() {
+    RegForm.style.transform = 'translateX(300px)';
+    LoginForm.style.transform = 'translateX(300px)';
+    Indicator.style.transform = 'translateX(0px)';
+}
+
+
